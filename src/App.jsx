9c2b989042e163adadc36750c4c05cd6f89db7a3
SNelson723 <stephenn649@gmail.com>
2024-12-03 07:08:31 -0600

    Setup the React with Vite project and working on building the basic components for the application. It's a simple SPA so only frontend will be in this application.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/TrackComponents/TrackList.jsx
@@ -0,0 +1,5 @@
+const TrackList = () => {
+
+};
+
+export default TrackList;
